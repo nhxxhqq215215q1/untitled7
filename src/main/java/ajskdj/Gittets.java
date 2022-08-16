e64b4c0983a3181107fd793c640c54584604dc9d
xxh <xxh@qq.com>
2022-08-16 15:27:24 +0800

    second commit

---
+++ src/main/java/ajskdj/Gittets.java
@@ -0,0 +1,15 @@
+package ajskdj;
+
+import org.junit.Test;
+
+public class Gittets {
+    @Test
+    public void test1()
+    {
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+
+    }
+}
